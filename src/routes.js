eb97cb2db9915c4e1fdaa0890495e3b43966cd85
rodrigoferraro <ferraro.rodrigo@gmail.com>
2019-12-25 00:48:50 -0300

    eslint + prettier (airbnb std - configured)

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res)=>{
-  return res.json({ message: 'Oilà Mondo! App responding on :3333 ok.'})
-})
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Oilà Mondo! App responding on :3333 ok.' });
+});
+
 export default routes;
