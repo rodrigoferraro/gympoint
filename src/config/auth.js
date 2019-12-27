cf72f2f91a4d421a60fe2466edb1b30d97ddf799
rodrigoferraro <ferraro.rodrigo@gmail.com>
2019-12-26 23:28:56 -0300

    autenticando admin using jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ac7012bece852820df8bab0d4dc01e4e',
+  expiresIn: '1d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Oilà Mondo! App responding on :3333 ok.' });
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
