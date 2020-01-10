0dacc552646083345f2249f0bc999e10f3630b68
rodrigoferraro <ferraro.rodrigo@gmail.com>
2020-01-09 22:37:46 -0300

    checkin table adjusted

--- src/database/migrations/20200109234931-create-checkins.js
+++ src/database/migrations/20200109234931-create-checkins.js
@@ -5,6 +5,9 @@ module.exports = {
       student_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        references: { model: 'students', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
       },
       created_at: {
         type: Sequelize.DATE,
