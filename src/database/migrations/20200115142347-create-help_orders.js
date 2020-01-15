e5cedac4e2acc76da1ac69309be5e6b2b16d6885
Rodrigo Ferraro <ferraro.rodrigo@gmail.com>
2020-01-15 13:00:45 -0300

    create help_order migration

---
+++ src/database/migrations/20200115142347-create-help_orders.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('help_orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'students', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      question: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      answer: {
+        type: Sequelize.TEXT,
+      },
+      answered_at: {
+        type: Sequelize.DATEONLY,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('help_orders');
+  },
+};
